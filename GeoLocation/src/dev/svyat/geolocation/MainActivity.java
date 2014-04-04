945b464dbc5a51ece959d6266ef48e1c7b0d19c7
Svyatoslav O Reshetnikov <svyatoslav.reshetnikov@gmail.com>
2014-04-04 12:41:10 +0300

    When user launch application, Map centering on user. I changed MainActivity.java.

--- GeoLocation/src/dev/svyat/geolocation/MainActivity.java
+++ GeoLocation/src/dev/svyat/geolocation/MainActivity.java
@@ -1,20 +1,34 @@
 package dev.svyat.geolocation;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener{
 
 	// Google Map
     private GoogleMap googleMap;
+    // Global variable to hold the current location
+    Location mCurrentLocation;
+    LocationClient mLocationClient;
+    double lat;
+    double lon;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +82,28 @@ public class MainActivity extends ActionBarActivity {
             }
             else{
             	googleMap.setMyLocationEnabled(true);
-            	googleMap.getUiSettings().setZoomControlsEnabled(false);
-            	googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            	googleMap.getUiSettings().setZoomControlsEnabled(true);
+            	googleMap.getUiSettings().setZoomGesturesEnabled(true);
+            	googleMap.getUiSettings().setCompassEnabled(true);
+            	
+            	lat = 0.0;
+            	lon = 0.0;
+
+            	googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            		
+            		@Override
+                    public void onMyLocationChange(Location location) {
+            			if(lat == 0.0 && lon == 0.0){
+            				lat = location.getLatitude();
+            				lon = location.getLongitude();
+            				googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).title("It's You"));
+                			googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 15));
+            			}
+            			else{
+            				googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lon)).title("It's You"));
+            			}
+            		}
+            	});
             }
         }
     }
@@ -79,5 +113,23 @@ public class MainActivity extends ActionBarActivity {
         super.onResume();
         initilizeMap();
     }
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		
+		
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		
+		
+	}
 
 }
